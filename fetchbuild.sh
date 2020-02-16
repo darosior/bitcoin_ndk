45d9eb81dab2ad6ecc19ca0bf7385b4402e70398
darosior <darosior@protonmail.com>
2020-02-16 21:26:33 +0100

    Don't even build core, we're only interested in cln

--- fetchbuild.sh
+++ fetchbuild.sh
@@ -102,85 +102,6 @@ deactivate
 cd ..
 
 
-export CFLAGS="-flto"
-export LDFLAGS="$CFLAGS -pie -static-libstdc++ -fuse-ld=lld"
-# build core
-git clone $repo ${reponame}
-cd ${reponame}
-git checkout $commit
-patch -p1 < /repo/0001-android-patches.patch
-
-if [ -f /repo/0001-android-patches-${commit}.patch ]; then
-    patch -p1 < /repo/0001-android-patches-${commit}.patch
-fi
-
-(cd depends && make HOST=${target_host/v7a/} NO_QT=1 -j ${num_jobs})
-./autogen.sh
-./configure --prefix=$PWD/depends/${target_host/v7a/} ac_cv_c_bigendian=no ac_cv_sys_file_offset_bits=$bits --disable-bench --enable-experimental-asm --disable-tests --disable-man --without-utils --enable-util-cli --without-libs --with-daemon --disable-maintainer-mode --disable-glibc-back-compat ${configextra}
-make -j ${num_jobs}
-make install
-$STRIP depends/${target_host/v7a/}/bin/${reponame}d
-cd ..
-
-# build tor deps
-TORBUILDROOT=$PWD/tor_build_root
-mkdir $TORBUILDROOT
-
-# build libevent
-unpackdep https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz 229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e
-cd libevent-release-2.1.11-stable
-./autogen.sh
-./configure --prefix=$TORBUILDROOT/libevent --enable-static --disable-samples \
-            --disable-openssl --disable-shared --disable-libevent-regress --disable-debug-mode \
-            --disable-dependency-tracking --host $target_host
-make -o configure install -j${num_jobs}
-cd ..
-
-# build zlib
-unpackdep https://github.com/madler/zlib/archive/v1.2.11.tar.gz 629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff
-cd zlib-1.2.11
-
-./configure --static --prefix=$TORBUILDROOT/zlib
-make -o configure install -j${num_jobs}
-cd ..
-
-
-# build openssl
-unpackdep https://github.com/openssl/openssl/archive/OpenSSL_1_1_1d.tar.gz 23011a5cc78e53d0dc98dfa608c51e72bcd350aa57df74c5d5574ba4ffb62e74
-
-cd openssl-OpenSSL_1_1_1d
-SSLOPT="no-gost no-shared no-dso no-ssl3 no-idea no-hw no-dtls no-dtls1 \
-        no-weak-ssl-ciphers no-comp -fvisibility=hidden no-err no-psk no-srp"
-
-if [ "$bits" = "64" ]; then
-    SSLOPT="$SSLOPT enable-ec_nistp_64_gcc_128"
-fi
-./Configure android-$NDKARCH --prefix=$TORBUILDROOT/openssl $SSLOPT
-make depend
-make -j${num_jobs} 2> /dev/null
-make install_sw
-cd ..
-
-
-# build tor
-unpackdep https://github.com/torproject/tor/archive/tor-0.4.2.5.tar.gz 94ad248f4d852a8f38bd8902a12b9f41897c76e389fcd5b8a7d272aa265fd6c9
-cd tor-tor-0.4.2.5
-./autogen.sh
-TOROPT="--disable-system-torrc --disable-asciidoc --enable-static-tor --enable-static-openssl \
-        --with-zlib-dir=$TORBUILDROOT/zlib --disable-systemd --disable-zstd \
-        --enable-static-libevent --enable-static-zlib --disable-system-torrc \
-        --with-openssl-dir=$TORBUILDROOT/openssl --disable-unittests \
-        --with-libevent-dir=$TORBUILDROOT/libevent --disable-lzma \
-        --disable-tool-name-check --disable-rust \
-        --disable-largefile ac_cv_c_bigendian=no \
-        --disable-module-dirauth"
-
-./configure $TOROPT --prefix=$TORBUILDROOT/tor --host=$target_host --disable-android
-make -o configure install -j${num_jobs}
-$STRIP $TORBUILDROOT/tor/bin/tor
-mv $TORBUILDROOT/tor/bin/tor ../${reponame}/depends/${target_host/v7a/}/bin
-cd ..
-
 # packaging
 if [ "${reponame}" != "${rename}" ]; then
     mv ${reponame}/depends/${target_host/v7a/}/bin/${reponame}d ${reponame}/depends/${target_host/v7a/}/bin/${rename}d
@@ -189,7 +110,7 @@ if [ "${reponame}" != "${rename}" ]; then
 else
     outputtar=/repo/${target_host/v7a/}_$(basename $(dirname ${repo})).tar
 fi
-tar -cf ${outputtar} -C ${reponame}/depends/${target_host/v7a/}/bin ${rename}d ${rename}-cli tor
+# tar -cf ${outputtar} -C ${reponame}/depends/${target_host/v7a/}/bin ${rename}d ${rename}-cli tor
 tar -rf ${outputtar} -C lightning/lightningd lightning_channeld lightning_closingd lightning_connectd lightning_gossipd lightning_hsmd lightning_onchaind lightning_openingd lightningd
 tar -rf ${outputtar} -C lightning plugins/autoclean plugins/fundchannel plugins/pay
 tar -rf ${outputtar} -C lightning/cli lightning-cli
